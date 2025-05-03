23ce877c0b5a6ae001d88416df8bb554479b4549
lakshaygupta2004 <laksh8403@gmail.com>
2025-05-03 17:27:08 +0530

    deployed

--- frontend/src/context/appState.js
+++ frontend/src/context/appState.js
@@ -3,9 +3,8 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 // 192.168.0.104
 //http://192.168.0.104:5000
-//https://chat-app-u2cq.onrender.com
 // http://localhost:5000
-const hostName = "http://localhost:5000";
+const hostName = "https://quissel.onrender.com";
 var socket = io(hostName);
 
 const ChatState = (props) => {
