268c9d301f08ab796163e40a0e978e4c88bd970d
lakshaygupta2004 <laksh8403@gmail.com>
2025-05-04 15:04:26 +0530

    error handled

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ const http = require("http");
 const PORT = 5000;
 const { initSocket } = require("./socket/index.js");
 const app = express();
-app.use(cors({
-  origin: 'https://your-frontend.vercel.app', // Replace with your actual frontend URL
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 
